2d531ab4561b891b6d62b16593f570788264e2ed
Evgeniia Mokhova <kochetkovaev11@gmail.com>
2024-09-07 21:45:21 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
+import { defineConfig } from "vite"
 import svgr from "vite-plugin-svgr"
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/Goods4you/",
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, "src"),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-        @import "src/assets/styles/mixins.scss";
-        @import "src/assets/styles/variables.scss";`,
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/Goods4you/" : "/",
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, "src"),
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "src/assets/styles/mixins.scss";
+            @import "src/assets/styles/variables.scss";`,
+        },
+      },
+    },
+  }
 })
